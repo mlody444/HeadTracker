aba84026ae307637b8fb9eb316a05e86fa696d4d
mlody444 <44815771+mlody444@users.noreply.github.com>
2024-09-29 19:52:03 +0200

    Additional functionalities: shape, del point, del all points (#15)

--- firmware/src/src/AR/position.h
+++ firmware/src/src/AR/position.h
@@ -1,15 +1,9 @@
 #ifndef POSITION_HH
 #define POSITION_HH
 
-#define MAX_NAME_LENGTH 16
+#include "common_ar.h"
 
-enum Point_Type_T {
-    DIAMOND,
-    SQUARE,
-    CIRCLE,
-    TRIANGLE,
-    X_SHAPE
-};
+#define MAX_NAME_LENGTH 16
 
 struct Position_Data_T {
   float azimuth;
@@ -24,6 +18,7 @@ void position_set_roll(float tilt_new);
 void position_set_pitch(float roll_new);
 void position_set_azimuth(float pan_new);
 void position_add_point(struct Position_Data_T point_data);
+void position_add_point(char name[], uint8_t length, float azimuth, float pitch, uint32_t distance);
 void position_del_point(char *name, uint8_t size);
 void position_Thread();
 
